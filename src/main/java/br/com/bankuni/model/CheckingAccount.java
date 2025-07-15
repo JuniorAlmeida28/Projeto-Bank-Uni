0753436fd78a30a549163642ca26042e86057b17
Junior Almeida <jrjunioralmeida@outlook.com>
2025-07-15 15:33:18 -0300

    Criação da classe CheckingAccount

--- src/main/java/br/com/bankuni/model/CheckingAccount.java
+++ src/main/java/br/com/bankuni/model/CheckingAccount.java
@@ -1,2 +1,17 @@
-package main.java.br.com.bankuni.model;public class CheckingAccount {
+package main.java.br.com.bankuni.model;
+
+public class CheckingAccount extends Account{
+
+    public CheckingAccount(String number, Client client) {
+        super(number, client);
+    }
+
+    @Override
+    public void withdraw(double value) {
+        if (value <= balance){
+            balance += value;
+        }else {
+            throw new IllegalArgumentException("Saldo Insuficiente");
+        }
+    }
 }
