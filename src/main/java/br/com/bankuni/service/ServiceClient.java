d73101d38d7c50d4b63aa2168238bacf1a0bc33b
Junior Almeida <jrjunioralmeida@outlook.com>
2025-07-17 15:39:17 -0300

    Criação da classe ServiceClient

--- src/main/java/br/com/bankuni/service/ServiceClient.java
+++ src/main/java/br/com/bankuni/service/ServiceClient.java
@@ -1,2 +1,23 @@
-package main.java.br.com.bankuni.service;public class ServiceClient {
+package main.java.br.com.bankuni.service;
+
+import main.java.br.com.bankuni.model.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceClient {
+    private List<Client> clients = new ArrayList<>();
+
+    public void newClient(Client client){
+        clients.add(client);
+    }
+
+    public Client findclientByCpf(String cpf){
+        for (Client cl : clients){
+            if (cl.getCpf().equals(cpf)){
+                return cl;
+            }
+        }
+        return null;
+    }
 }
